8db97308935ac17c149a5cc472f7d92098125c37
Qcoder <3185566911@qq.com>
2026-05-11 20:07:19 +0800

    Add files via upload

---
+++ hot_100_279_完全平方数.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n + 1, n + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j * j <= i; ++j)
+            {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+
+            }
+        }
+        return dp[n];
+
+    }
+};
