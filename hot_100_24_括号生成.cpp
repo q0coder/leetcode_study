2d70cc44235aea2bb3729ca5ed8cabb3df37f950
Qcoder <3185566911@qq.com>
2026-06-07 18:58:42 +0800

    Add files via upload

---
+++ hot_100_24_括号生成.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string path(n * 2, 0); // 所有括号长度都是一样的 2n
+
+        // 目前填了 left 个左括号，right 个右括号
+        auto dfs = [&](this auto&& dfs, int left, int right) -> void {
+            if (right == n) { // 填完 2n 个括号
+                ans.emplace_back(path);
+                return;
+            }
+            if (left < n) { // 可以填左括号
+                path[left + right] = '('; // 直接覆盖
+                dfs(left + 1, right);
+            }
+            if (right < left) { // 可以填右括号
+                path[left + right] = ')'; // 直接覆盖
+                dfs(left, right + 1);
+            }
+        };
+
+        dfs(0, 0); // 一开始没有填括号
+        return ans;
+    }
+};
+
